882a6673a68e76d09b9994fcec8a98afe415e442
krismile37 <50942544+krismile37@users.noreply.github.com>
2019-12-27 17:47:37 +0500

    Update 1.php

--- PHP/1.php
+++ PHP/1.php
@@ -1,5 +1,10 @@
 /* Найдите ошибки в приведенном ниже исходном тексте программы на РНP. */
 
+<? php 
+print 'How are you? '; 
+print 'I' m fine. '; 
+??> 
+
 <?php
     print 'How are you?';
     print "I'm fine. ";
